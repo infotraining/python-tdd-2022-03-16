fd861801075e2d2c5a96b8930d7d3a7a0b297e15
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-03-17 10:00:27 +0100

    Mars Rover - done

--- kata/mars-rover/test_mars_rover.py
+++ kata/mars-rover/test_mars_rover.py
@@ -1,12 +1,64 @@
 import pytest
 
 
+class UnboundedGrid:
+    def wrap(self, x, y):
+        return (x, y)
+
+
+def test_UnboundedGrid_returns_unchanged_coordinates():
+    grid = UnboundedGrid()
+    assert grid.wrap(100, 100) == (100, 100)
+
+
+class Grid:
+    def __init__(self, max_width, max_height) -> None:
+        self.max_width = max_width
+        self.max_height = max_height
+
+    def wrap(self, x, y):
+        return (x % self.max_width, y % self.max_height)
+
+
+def test_Grid_when_overflow_wraps_the_coordinates():
+    grid = Grid(5, 5)
+
+    assert grid.wrap(5, 5) == (0, 0)
+    assert grid.wrap(6, 6) == (1, 1)
+
+
 class Rover:
-    def __init__(self, position, direction) -> None:
+    def __init__(self, position, direction, grid=UnboundedGrid()) -> None:
         self.position = position
         self.direction = direction
+        self.grid = grid
         self.clockwise_direction_sequence = ['E', 'S', 'W', 'N']
         self.counterclockwise_direction_sequence = self.clockwise_direction_sequence[::-1]
+        self._longitudinal_strides = {
+            "N": (0, 1), "S": (0, -1), "E": (1, 0), "W": (-1, 0)}
+        self.FORWARD = 1
+        self.BACKWARD = -1
+
+    def _move(self, sense):
+        x, y = self.position
+        hor_stride, ver_stride = self._longitudinal_strides[self.direction]
+        self.position = self.grid.wrap(
+            x + hor_stride * sense, y + ver_stride * sense)
+
+    def move_forward(self):
+        self._move(self.FORWARD)
+
+    def move_backward(self):
+        self._move(self.BACKWARD)
+    
+    def move(self, command_sequence):
+        command_abbreviations = {"F": self.move_forward, "B": self.move_backward,
+                                 "R": self.turn_right, "L": self.turn_left}
+        for abbreviation in command_sequence:
+            try:
+                command_abbreviations[abbreviation]()
+            except KeyError:
+                raise ValueError('Command unknown')
 
     def turn_left(self):
         index = self.clockwise_direction_sequence.index(self.direction)
@@ -16,8 +68,6 @@ class Rover:
         index = self.counterclockwise_direction_sequence.index(self.direction)
         self.direction = self.counterclockwise_direction_sequence[index - 1]
 
-    def move_forward(self):
-        pass
 
 def test_Rover_accepts_coordinates_and_direction():
     rover = Rover(position=(1, 1), direction="N")
@@ -62,3 +112,32 @@ def test_Rover_moves_forward(start_position, start_direction, end_position):
     rover.move_forward()
     assert rover.position == end_position
     assert rover.direction == start_direction
+
+
+@pytest.mark.parametrize("start_position,start_direction,end_position", [
+    ((1, 4), 'N', (1, 0)),
+    ((4, 1), 'E', (0, 1)),
+    ((1, 0), 'S', (1, 4)),
+    ((0, 1), 'W', (4, 1))
+])
+def test_Rover_moves_forward_and_wraps(start_position, start_direction, end_position):
+    rover = Rover(position=start_position,
+                  direction=start_direction, grid=Grid(5, 5))
+
+    rover.move_forward()
+    assert rover.position == end_position
+    assert rover.direction == start_direction
+
+
+def test_Rover_moves_using_command_sequence():
+    command_sequence = "FFRFFBFLR"
+    rover = Rover(position=(0, 0), direction="N")
+    rover.move(command_sequence)
+    assert rover.position == (2, 2)
+    assert rover.direction == "E"
+
+def test_Rover_raises_ValueError_with_wrong_sequence():
+    command_sequence = "xFFRFFBFLR"
+    rover = Rover(position=(0, 0), direction="N")
+    with pytest.raises(ValueError):
+        rover.move(command_sequence)
